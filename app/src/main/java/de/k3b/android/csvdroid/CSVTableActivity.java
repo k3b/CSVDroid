50ba239479aed2984f5803e87d88bc7f262cf727
k3b <1374583+k3b@users.noreply.github.com>
2023-03-24 09:44:49 +0100

    initial version generated for api-19

---
+++ app/src/main/java/de/k3b/android/csvdroid/CSVTableActivity.java
@@ -0,0 +1,14 @@
+package de.k3b.android.csvdroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CSVTableActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_csv_table);
+    }
+}
